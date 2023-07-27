0e632fede0ee9aa6e8b17b7f76a1f6ff17355d29
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2023-07-27 18:53:06 +0300

    added dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import express from 'express'
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const app = express();
-const port = 5000;
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+const port = process.env.PORT || 5000;
+
+const app: Application = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
+
+app.get('/health', (req, res) => {
+  res.send({ status: 200, message: 'Up & Running' });
+});
+
 app.listen(port, () => console.log(`Running on port ${port}`));
